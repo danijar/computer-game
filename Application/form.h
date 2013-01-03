b719a15a4acf2adfece7dcd4ce4714a0861cdd48
Danijar Hafner <pfisrik@gmail.com>
2013-01-03 19:47:34 +0100

    Texture Support Added
    replaced color of forms by texture support
    added two non progressive jpeg images as example textures

--- Application/form.h
+++ Application/form.h
@@ -8,7 +8,7 @@ using namespace glm;
 
 struct StorageForm : public Storage
 {
-	unsigned int VerticesPosition, VerticesColor, Elements, Program;
+	unsigned int Positions, Texcoords, Elements, Program, Texture;
 	vec3 Scale;
-	StorageForm() : VerticesPosition(-1), VerticesColor(-1), Elements(-1), Program(-1), Scale(1) {}
+	StorageForm() : Scale(1) {}
 };
--- Application/shader.h
+++ Application/shader.h
@@ -7,7 +7,7 @@ using namespace sf;
 
 struct StorageShader : public Storage
 {
-	GLuint Vertex, Fragment, Program;
-	GLint Position, Color, Model, View, Projection;
-	StorageShader() : Vertex(-1), Fragment(-1), Program(-1), Position(-1), Color(-1), Model(-1) {}
+	GLuint Vertex, Fragment, Program,			// shaders
+		   Position, Texcoord,					// inputs
+	       Model, View, Projection, Texture;	// uniforms
 };
