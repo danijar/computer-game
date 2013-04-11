22c88355639c86abb34cf8cebb693df414200024
Danijar Hafner <pfisrik@gmail.com>
2013-04-11 17:41:09 +0200

    Added Hello World of Bullet Physics

--- Application/physic/module.cpp
+++ Application/physic/module.cpp
@@ -1,15 +1,39 @@
 #include "module.h"
 
+#include <string>
+using namespace std;
+
 #include "transform.h"
 
 
 void ModulePhysic::Init()
 {
+	broadphase = new btDbvtBroadphase();
+	configuration = new btDefaultCollisionConfiguration();
+	dispatcher = new btCollisionDispatcher(configuration);
+	solver = new btSequentialImpulseConstraintSolver;
+	world = new btDiscreteDynamicsWorld(dispatcher, broadphase, solver, configuration);
 
+	world->setGravity(btVector3(0, -10, 0));
+
+	Ground();
+	Cube();
 }
 
 void ModulePhysic::Update()
 {
+	float delta = clock.restart().asSeconds();
+	world->stepSimulation(delta, 10);
+ 
+	btTransform trans;
+	shape->getMotionState()->getWorldTransform(trans);
+	float newHeight = trans.getOrigin().getY();
+	if(newHeight != shapeHeight)
+	{
+		shapeHeight = newHeight;
+		Debug->Print("shape hight " + to_string(shapeHeight));
+	}
+
 	auto tfs = Entity->Get<StorageTransform>();
 	for(auto i = tfs.begin(); i != tfs.end(); ++i)
 	{
@@ -23,4 +47,13 @@ void ModulePhysic::Update()
 			Matrix(i->first);
 		}
 	}
+}
+
+ModulePhysic::~ModulePhysic()
+{
+	delete world;
+	delete solver;
+	delete dispatcher;
+	delete configuration;
+	delete broadphase;
 }
--- Application/physic/module.h
+++ Application/physic/module.h
@@ -2,12 +2,32 @@
 
 #include "system.h"
 
+#include <SFML/System.hpp>
+#include <BULLET/btBulletDynamicsCommon.h>
+
 
 class ModulePhysic : public Module
 {
+	// general
+	sf::Clock clock;
 	void Init();
 	void Update();
-	void Matrix(unsigned int id);
+	~ModulePhysic();
 
-	void Test();
+	// bullet
+	btBroadphaseInterface* broadphase;
+	btDefaultCollisionConfiguration* configuration;
+	btCollisionDispatcher* dispatcher;
+	btSequentialImpulseConstraintSolver* solver;
+	btDiscreteDynamicsWorld* world;
+	
+	// test
+	btRigidBody* ground;
+	btRigidBody* shape;
+	float shapeHeight;
+	void Ground();
+	void Cube();
+
+	// transform
+	void Matrix(unsigned int id);
 };
