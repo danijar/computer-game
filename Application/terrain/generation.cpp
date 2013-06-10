ed281bf69712dccadab759a936bffb11b4266787
Danijar Hafner <pfisrik@gmail.com>
2013-06-10 14:20:55 +0200

    improved and tweaked overhangs
    density caused blocks are made recognizable by block type

--- Application/terrain/generation.cpp
+++ Application/terrain/generation.cpp
@@ -18,20 +18,30 @@ void ModuleTerrain::Generate(Terrain *Terrain)
 		for(int z = 0; z < CHUNK_SIZE.z; ++z)
 		{
 			// two dimensional
-			vector<float> layers;
-			layers.push_back(0.3f);
-			layers.push_back(0.5f * (simplex(0.375f / maxground * vec2(offset.x + x, offset.z + z))) / 2 + 0.5f);
-			layers.push_back(0.2f * (simplex(1.500f / maxground * vec2(offset.x + x, offset.z + z))) / 2 + 0.5f);
-			float height = 0; for(auto i : layers) height += i;
+
+			vector<float> heightmaps;
+			heightmaps.push_back(0.3f);
+			heightmaps.push_back(0.5f * (simplex(0.4f / maxground * vec2(offset.x + x, offset.z + z))) / 2 + 0.5f);
+			heightmaps.push_back(0.2f * (simplex(1.5f / maxground * vec2(offset.x + x, offset.z + z))) / 2 + 0.5f);
+			float heightmap = 0; for(auto i : heightmaps) heightmap += i;
+
+			float densityamount = 1 / (1 + pow(10000, simplex(0.3f / maxground * vec2(offset.x + x, offset.z + z)) + 0.5f));
 
 			for(int y = 0; y < CHUNK_SIZE.y; ++y)
 			{
 				// three dimensional
-				float gradient = -y / height / maxground + 1;
-				float density = 0.3f * (simplex(2.000f / maxground * vec3(offset.x + x, offset.y + y, offset.z + z)) / 1.5f + 0.3f);
 
-				if(0.0f < gradient + density)
-					Terrain->Blocks[x][y][z] = rand() % 2 + 1;
+				float gradient = -y / heightmap / maxground + 1;
+
+				vector<float> densities;
+				if(y > heightmap * maxground && y < heightmap * maxground * 1.2f)
+					densities.push_back(0.5f * simplex(1.5f / maxground * vec3(offset + ivec3(x, y, z))));
+				//densities.push_back(1.0f * (simplex(0.9f / maxground * vec3(offset.x + x, offset.y + y, offset.z + z)) / 1.5f + 0.3f));
+				//densities.push_back(0.5f * (simplex(2.0f / maxground * vec3(offset.x + x, offset.y + y, offset.z + z)) / 2));
+				float density = 0; for(auto i : densities) density += i;
+
+				if(0.0f < gradient + density * densityamount)
+					Terrain->Blocks[x][y][z] = gradient > density ? rand() % 2 + 1 : 3;
 			}
 		}
 	}
