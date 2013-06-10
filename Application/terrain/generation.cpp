e64d2112aae65fe7e8e3efb71bb0af62f25b7ce4
Danijar Hafner <pfisrik@gmail.com>
2013-06-10 17:30:38 +0200

    more mountains with overhangs and passages

--- Application/terrain/generation.cpp
+++ Application/terrain/generation.cpp
@@ -25,23 +25,24 @@ void ModuleTerrain::Generate(Terrain *Terrain)
 			heightmaps.push_back(0.2f * (simplex(1.5f / maxground * vec2(offset.x + x, offset.z + z))) / 2 + 0.5f);
 			float heightmap = 0; for(auto i : heightmaps) heightmap += i;
 
-			float densityamount = 1 / (1 + pow(10000, simplex(0.3f / maxground * vec2(offset.x + x, offset.z + z)) + 0.5f));
+			float densityamount = 1 / (1 + (float)pow(10000, simplex(0.3f / maxground * vec2(offset.x + x, offset.z + z)) + 0.5f));
 
 			for(int y = 0; y < CHUNK_SIZE.y; ++y)
 			{
 				// three dimensional
 
 				float gradient = -y / heightmap / maxground + 1;
+				if(gradient < 0) gradient = -(gradient * gradient);
 
 				vector<float> densities;
 				if(y > heightmap * maxground && y < heightmap * maxground * 1.2f)
 					densities.push_back(0.5f * simplex(1.5f / maxground * vec3(offset + ivec3(x, y, z))));
-				//densities.push_back(1.0f * (simplex(0.9f / maxground * vec3(offset.x + x, offset.y + y, offset.z + z)) / 1.5f + 0.3f));
-				//densities.push_back(0.5f * (simplex(2.0f / maxground * vec3(offset.x + x, offset.y + y, offset.z + z)) / 2));
+				densities.push_back(0.2f * (simplex(0.9f / maxground * vec3(offset.x + x, offset.y + y, offset.z + z)) / 1.5f + 0.3f));
+				densities.push_back(0.2f * (simplex(2.0f / maxground * vec3(offset.x + x, offset.y + y, offset.z + z)) / 2));
 				float density = 0; for(auto i : densities) density += i;
 
 				if(0.0f < gradient + density * densityamount)
-					Terrain->Blocks[x][y][z] = gradient > density ? rand() % 2 + 1 : 3;
+					Terrain->Blocks[x][y][z] = gradient > 0 ? rand() % 2 + 1 : 3;
 			}
 		}
 	}
