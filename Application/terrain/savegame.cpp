95ea5249045f323a8ab6395030dab8d387d518f5
Danijar Hafner <pfisrik@gmail.com>
2013-06-25 13:23:38 +0200

    fixed loadinf and saving of chunks

--- Application/terrain/savegame.cpp
+++ Application/terrain/savegame.cpp
@@ -1,9 +1,11 @@
 #include "module.h"
 
 #include <string>
+#include <filesystem>
 #include <GLM/glm.hpp>
 #include <ZIP/zipint.h>
 using namespace std;
+using namespace std::tr2::sys;
 using namespace glm;
 
 bool ModuleTerrain::Load(string File, Terrain *Terrain)
@@ -18,11 +20,18 @@ bool ModuleTerrain::Load(string File, Terrain *Terrain)
 	zip_stat_init(&stat);
 	int result = zip_stat(archive, name.c_str(), 0, &stat);
 	if(result < 0)
+	{
+		zip_close(archive);
 		return false;
+	}
 
 	zip_file *file = zip_fopen(archive, name.c_str(), 0);
 	if(file == NULL)
+	{
+		zip_fclose(file);
+		zip_close(archive);
 		return false;
+	}
 
 	zip_fread(file, Terrain->Blocks, stat.size);
 	zip_fclose(file);
@@ -34,31 +43,36 @@ bool ModuleTerrain::Load(string File, Terrain *Terrain)
 
 bool ModuleTerrain::Save(string File, Terrain *Terrain)
 {
+	path dir(Name() + "/save");
+	if(!exists(dir)) create_directory(dir);
+
 	string name = to_string(Terrain->Key.x) + "," + to_string(Terrain->Key.y) + "," + to_string(Terrain->Key.z) + ".txt";
 
 	zip *archive = zip_open((Name() + "/save/" + File +".zip").c_str(), ZIP_CREATE, NULL);
 	if(archive == NULL)
-	{
-		Debug->Fail("could not open world file " + string(zip_strerror(archive)));
 		return false;
-	}
 
 	struct zip_source *source = zip_source_buffer(archive, Terrain->Blocks, sizeof Terrain->Blocks, 0);
 	if(source == NULL)
 	{
 		zip_source_free(source);
-		Debug->Fail("could not store chunk to disk " + string(zip_strerror(archive)));
+		zip_close(archive);
 		return false;
 	}
 
-	zip_int64_t index =  zip_file_add(archive, name.c_str(), source, ZIP_FL_OVERWRITE);
+	zip_int64_t index = zip_file_add(archive, name.c_str(), source, ZIP_FL_OVERWRITE);
 	if(index < 0)
 	{
-		Debug->Fail("could not store chunk to disk " + string(zip_strerror(archive)));
+		zip_close(archive);
 		return false;
 	}
 
-	zip_close(archive);
+	int result = zip_close(archive);
+	if(result < 0)
+	{
+		Debug->Fail("zip_close: " + string(zip_strerror(archive)));
+		return false;
+	}
 
 	return true;
 }
