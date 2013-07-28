a543c279815ebbf192043f4c60759fa22179b435
Danijar Hafner <pfisrik@gmail.com>
2013-07-28 14:38:35 +0200

    fixed batch to remove source for distribution

--- remove source.bat
+++ remove source.bat
@@ -1,8 +1,9 @@
 @echo off
-@Are you sure?
+echo Are you sure?
 pause
 
 cd Application
+
 for /r %%i in (*) do ( if %%~xi==.cpp ( del %%i ) )
 for /r %%i in (*) do ( if %%~xi==.h   ( del %%i ) )
 for /f "usebackq" %%d in (`"dir /ad/b/s | sort /R"`) do rd "%%d"
